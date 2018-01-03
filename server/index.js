b6c5b72d169f4b931606d0f80bdc8cc8f7a1899c
Gustavo Asenjo <Tavo@Tavos-MBP.local>
2018-01-03 12:22:15 -0500

    Setting up Heroku

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('This is PSG');
+	res.send('This is PSG :)');
 });
 
 const PORT = process.env.PORT || 1111;
