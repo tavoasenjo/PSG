3659eb804b69dde19054fa1f92cdb30544f693bb
Gustavo Asenjo <Tavo@Tavos-MBP.local>
2018-01-03 18:10:19 -0500

    testing server and client

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const frontend = require('./client/src/App');
 
 const app = express();
 
-app.get('/test', (req, res) => {
-	res.send('This is PSG 🤹🏽‍♂️');
+app.get('/', (req, res) => {
+	res.send(frontend);
 });
 const PORT = process.env.PORT || 1111;
-app.listen(PORT); // app.listen(1111);
+app.listen(PORT);
+
+// app.listen(1111);
