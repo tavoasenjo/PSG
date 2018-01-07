66af334fb5bef9cc8ade2e4e4e899b9734626941
Gustavo Asenjo <Tavo@Tavos-MBP.local>
2018-01-07 14:52:30 -0500

    Initial Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+	return (
+		<div>
+			<h2>This is the Header</h2>
+		</div>
+	);
+};
+
+export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,28 @@
+/* eslint-disable import/first */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+	border: 2px solid red;
+	background-color: lightblue;
+	width: 100%;
+`;
+
+// import materialize CSS
+import 'materialize-css/dist/css/materialize.min.css';
+
+// import components
+import Header from './components/Header';
 
 class App extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>Hola Tavo!!!!</h1>
-			</div>
+			<Wrapper>
+				<div className="container">
+					<Header />
+				</div>
+			</Wrapper>
 		);
 	}
 }
